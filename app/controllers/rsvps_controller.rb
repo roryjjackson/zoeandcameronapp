6099c3e83b83d25b06e7073235489ee679a4f812
rory jackson <roryjamesjackson@hotmail.com>
2022-09-27 14:22:29 +0200

    Added links to the pages and the display page for rsvp

--- app/controllers/rsvps_controller.rb
+++ app/controllers/rsvps_controller.rb
@@ -1,25 +1,37 @@
 class RsvpsController < ApplicationController
   def index
-    @wedding = Wedding.find(params[:wedding_id])
-    @rsvps = Rsvp.where(wedding_id: @wedding.id)
-    @rsvp_yes = @rsvps.where(attendance: "Yes").order("created_at desc")
-    @rsvp_no = @rsvps.where(attendance: "No").order("created_at desc")
-    @total_participants = 0
-    @rsvps.each do |rsvp|
-      @total_participants += 1 if rsvp.attendance == "Yes"
-    end
+    # @rsvps = Rsvp.where(wedding_id: @wedding.id)
+    # @rsvp_yes = @rsvps.where(attendance: "Yes").order("created_at desc")
+    # @rsvp_no = @rsvps.where(attendance: "No").order("created_at desc")
+    # @total_participants = 0
+    # @rsvps.each do |rsvp|
+    #   @total_participants += 1 if rsvp.attendance == "Yes"
+    # end
+    @rsvps = Rsvp.all
 
   end
 
-  # def show
-  #   @rsvp = Rsvp.find(params[:id])
-  # end
+  def edit
+    @rsvp = Rsvp.find(:id)
+  end
+
+  def update
+    @rsvp = Rsvp.find(:id)
+    @vip.update(rsvp_params)
+  end
 
   def new
-    # @wedding_guests_id = WeddingGuest.find(params[:id])
-    # @wedding = Wedding.find(params[:wedding_id])
     @rsvp = Rsvp.new
-    # raise
+  end
+
+  def destroy
+    @rsvp = Rsvp.find(:id)
+    @rsvp.destroy
+    redirect_to root_path
+  end
+
+  def show
+    @rsvps = Rsvp.all
   end
 
   def create
--- app/models/rsvp.rb
+++ app/models/rsvp.rb
@@ -1,3 +1,3 @@
 class Rsvp < ApplicationRecord
-  belongs_to :user
+  # belongs_to :user
 end
